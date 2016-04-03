51d508d804991bf88368c1d74bce8f30e9369a0f
SleepWalker <vityagorbenko@yandex.ru>
2016-04-04 00:32:51 +0300

    Done module 9

---
+++ src/main/java/modulenine/CaesarEncryption.java
@@ -0,0 +1,116 @@
+package modulenine;
+
+/**
+ * Created by User on 03.04.2016.
+ */
+public class CaesarEncryption {
+    private final int ASCII_DIGIT_0 = 48;
+    private final int ASCII_UPPER_A = 65;
+    private final int ASCII_LOWER_A = 97;
+    private final int ALPHABET_LENGTH = 26;
+    private final int NUMERALS_SIZE = 10;
+    //private final int ASCII_SPECIAL_CHARACTER_FIRST = 32;
+    //private final int NUMBER_OF_SPECIAL_CHARACTERS = 16;
+
+    public String encode(String inputMessage, int key){
+        StringBuilder stringBuilder = new StringBuilder();
+        char characterReceived;
+
+        for(char character : inputMessage.toCharArray()){
+
+            if(Character.isLetter(character)){
+
+                if (Character.isUpperCase(character)){
+
+                    characterReceived = encodedCycleOfProcessingData(character, ASCII_UPPER_A, ALPHABET_LENGTH, key);
+                    stringBuilder.append(characterReceived);
+
+                } else if (Character.isLowerCase(character)){
+
+                    characterReceived = encodedCycleOfProcessingData(character, ASCII_LOWER_A, ALPHABET_LENGTH, key);
+                    stringBuilder.append(characterReceived);
+                }
+
+            } else if (Character.isDigit(character)){
+
+                characterReceived = encodedCycleOfProcessingData(character, ASCII_DIGIT_0, NUMERALS_SIZE, key);
+                stringBuilder.append(characterReceived);
+
+            } else {
+
+                stringBuilder.append(character);
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public String decode(String codedMessage, int key){
+        StringBuilder stringBuilder = new StringBuilder();
+        char characterReceived;
+
+        for (char character : codedMessage.toCharArray()){
+            if(Character.isLetter(character)){
+
+                if (Character.isUpperCase(character)){
+
+                    characterReceived = decodedCycleOfProcessingData(character, ASCII_UPPER_A, ALPHABET_LENGTH, key);
+                    stringBuilder.append(characterReceived);
+
+                } else if (Character.isLowerCase(character)){
+
+                    characterReceived = decodedCycleOfProcessingData(character, ASCII_LOWER_A, ALPHABET_LENGTH, key);
+                    stringBuilder.append(characterReceived);
+                }
+
+            } else if (Character.isDigit(character)){
+
+                characterReceived = decodedCycleOfProcessingData(character, ASCII_DIGIT_0, NUMERALS_SIZE, key);
+                stringBuilder.append(characterReceived);
+
+            } else  {
+
+                stringBuilder.append(character);
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public static char encodedCycleOfProcessingData(char character, int firstSymbolInASCII, int alphabetSize, int key){
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int asciiCodeOfCharacter = (character - firstSymbolInASCII - key) % alphabetSize;
+        int myCodeOfCharacter = asciiCodeOfCharacter + firstSymbolInASCII;
+
+        if (asciiCodeOfCharacter >= 0) {
+
+            stringBuilder.appendCodePoint(myCodeOfCharacter);
+        } else {
+
+            stringBuilder.appendCodePoint(myCodeOfCharacter + alphabetSize);
+        }
+
+        return stringBuilder.charAt(0);
+    }
+
+    public static char decodedCycleOfProcessingData(char character, int firstSymbolInASCII, int alphabetSize, int key){
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int asciiCodeOfCharacter = (character - firstSymbolInASCII - key + alphabetSize) % alphabetSize;
+        int myCodeOfCharacter = asciiCodeOfCharacter + firstSymbolInASCII;
+
+        if (asciiCodeOfCharacter >= 0) {
+
+            stringBuilder.appendCodePoint(myCodeOfCharacter);
+        } else {
+
+            stringBuilder.appendCodePoint(myCodeOfCharacter + alphabetSize);
+        }
+
+        return stringBuilder.charAt(0);
+    }
+
+
+
+}
---
+++ src/main/java/modulenine/Main.java
@@ -0,0 +1,42 @@
+package modulenine;
+
+import moduleeigth.AudioFile;
+import moduleeigth.Directory;
+import moduleeigth.File;
+
+/**
+ * Created by User on 03.04.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int encryptionCode = 13;
+        CaesarEncryption cryptographyExample = new CaesarEncryption();
+
+        File audioTrack1 = new AudioFile("", "Nigthwish", ".mp3", 323);
+        File audioTrack2 = new AudioFile("", "Linkin Park", ".mp3", 419);
+        File audioTrack3 = new AudioFile("", "AC/DC", ".mp3", 223);
+        File audioTrack4 = new AudioFile("", "Nickelback", ".mp3", 895);
+        File audioTrack5 = new AudioFile("", "Nirvana", ".mp3", 544);
+        File audioTrack6 = new AudioFile("", "Prodigy", ".mp3", 358);
+
+        Directory music = new Directory();
+        music.setDirectoryName("Music");
+
+        music.placeFilesToDirectory(audioTrack1);
+        music.placeFilesToDirectory(audioTrack2);
+        music.placeFilesToDirectory(audioTrack3);
+        music.placeFilesToDirectory(audioTrack4);
+        music.placeFilesToDirectory(audioTrack5);
+        music.placeFilesToDirectory(audioTrack6);
+
+        String encodedMessage = cryptographyExample.encode(music.getFiles().toString(), encryptionCode);
+        String decodedMessage = cryptographyExample.decode(encodedMessage, encryptionCode);
+
+        System.out.println(music.getFiles());
+        System.out.println(encodedMessage);
+        System.out.println(decodedMessage);
+
+
+
+    }
+}
