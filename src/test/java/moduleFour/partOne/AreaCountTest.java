b220937a37cef2116eb7df623fc6fd948031594d
SleepWalker <vityagorbenko@yandex.ru>
2016-04-07 02:44:53 +0300

    test

---
+++ src/test/java/moduleFour/partOne/AreaCountTest.java
@@ -0,0 +1,14 @@
+package moduleFour.partOne;
+
+import org.junit.Test;
+
+/**
+ * Created by User on 07.04.2016.
+ */
+public class AreaCountTest {
+
+    @Test
+    public void testTriangleSquare() throws Exception {
+
+    }
+}
