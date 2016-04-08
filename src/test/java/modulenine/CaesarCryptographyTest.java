05d7900db8bb950a4d25c43d770a4773158f81b3
SleepWalker <vityagorbenko@yandex.ru>
2016-04-08 04:31:10 +0300

    refactoring

--- src/test/java/modulenine/CaesarCryptographyTest.java
+++ src/test/java/modulenine/CaesarCryptographyTest.java
@@ -1,5 +1,6 @@
 package modulenine;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,14 +9,19 @@ import static org.junit.Assert.assertEquals;
  * Created by User on 08.04.2016.
  */
 public class CaesarCryptographyTest {
+    private static CaesarCryptography caesarCryptography;
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+
+        caesarCryptography = new CaesarCryptography();
+    }
 
     @Test
     public void testEncode() throws Exception {
         int key = 3;
         String string = "AbcD";
         String expectedString = "DefG";
-
-        CaesarCryptography caesarCryptography = new CaesarCryptography();
 
         String result = caesarCryptography.encode(string, key);
         assertEquals(expectedString, result);
@@ -27,8 +33,6 @@ public class CaesarCryptographyTest {
         int key = 2;
         String string = "CdEf";
         String expectedString = "AbCd";
-
-        CaesarCryptography caesarCryptography = new CaesarCryptography();
 
         String result = caesarCryptography.decode(string, key);
         assertEquals(expectedString, result);
